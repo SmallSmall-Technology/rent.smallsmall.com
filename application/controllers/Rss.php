c9059b635517a12c41623fa77fa39fa8e33f6fb7
Lawal Owolabi Yusuf <bwitlawalyusuf@gmail.com>
2023-07-14 03:55:11 +0100

    Update Rss.php
    Testing for missing values

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -7207,7 +7207,7 @@ public function email_test($lname = "RSS", $email = "seuncrowther@yahoo.com", $k
 
 	// Unione API Testing
 
-public function unione_template_getOLD()
+function unione_template_get()
 {
     require 'vendor/autoload.php';
 
@@ -7216,77 +7216,69 @@ public function unione_template_getOLD()
         'Accept' => 'application/json',
         'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
     );
-
+    
     $client = new \GuzzleHttp\Client([
         'base_uri' => 'https://eu1.unione.io/en/transactional/api/v1/'
     ]);
 
+    // Request body for retrieving the template
     $requestBody = [
-        "id" => "1cc035cc-0f2c-11ee-8166-821d93a29a48"
+        "id" => "1cc035cc-0f2c-11ee-8166-821d93a29a48",
     ];
 
     try {
-        $response = $client->request('POST', 'template/get.json', array(
+        // Retrieve the template from the Unione API
+        $response = $client->request('POST', 'template/get.json', [
             'headers' => $headers,
             'json' => $requestBody,
-        ));
+        ]);
 
-        $jsonResponse = $response->getBody()->getContents();
-        $responseData = json_decode($jsonResponse, true);
+        $responseData = json_decode($response->getBody()->getContents(), true);
 
+        // Get the HTML body from the template response
         $htmlBody = $responseData['template']['body']['html'];
-        
-        
-        // Get the unique username
-        // $user = $this->admin_model->get_user($id);
+
+        // Replace placeholders in the HTML body with actual values
         $username = "Yusuf";
         $resetLink = 'https://buy.rentsmallsmall.com/';
         $email = 'yusuf.i@smallsmall.com';
-        
-        // Replace the placeholder in the HTML body with the username
         $htmlBody = str_replace('{{Name}}', $username, $htmlBody);
         $htmlBody = str_replace('{{resetLink}}', $resetLink, $htmlBody);
 
+        // Prepare the email data
+        $emailData = [
+            "message" => [
+                "recipients" => [
+                    ["email" => $email],
+                ],
+                "body" => ["html" => $htmlBody],
+                "subject" => "Testing",
+                "from_email" => "donotreply@smallsmall.com",
+                "from_name" => "Smallsmall",
+            ],
+        ];
 
-        $data['response'] = $htmlBody;
+        // Send the email using the Unione API
+        $response = $client->request('POST', 'email/send.json', [
+            'headers' => $headers,
+            'json' => $emailData,
+        ]);
+
+        // Output the result
+        echo 'Email Sent successfully to ' . $email;
+        
     } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-        $data['response'] = $e->getMessage();
+        
+        // Handle API errors
+       print_r($e->getMessage());
+    } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+        // Handle other exceptions
+        print_r($e->getMessage());
     }
-    
-    // End Of Unione
-				
-		$this->email->from('donotreply@smallsmall.com', 'Small Small Password Reset');
-
-		$this->email->to($email);
-
-		$this->email->subject("Password Reset RentSmallsmall");	
-				
-		$this->email->set_mailtype("html");
-				
-		$message = $this->load->view('rss-partials/unione-testing', $data, TRUE);
-
-		$this->email->message($message);
-
-		$emailRes = $this->email->send();
-
-        // $this->load->view('rss-partials/unione-testing', $data);
-        
-        if($emailRes){
-					
-		echo 'Email Sent successfully to' . ''. $email;
-					
-	}else{
-					
-		echo 'Error sending Email';
-					
-		}
-        
-        
-        
 }
 
 
-public function unione_template_get()
+public function unione_template_getOLD()
 {
 
 require 'vendor/autoload.php';
