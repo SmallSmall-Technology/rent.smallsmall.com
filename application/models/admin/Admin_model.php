7fbdf7807b69c8072086abd7d51174a10660f02b
yusuf <bwitlawalyusuf@gmail.com>
2023-07-27 04:40:37 +0100

    countAppVerification updated

--- application/models/admin/Admin_model.php
+++ application/models/admin/Admin_model.php
@@ -190,7 +190,7 @@ class Admin_model extends CI_Model {
 	}
 	public function countAppVerifications() {
 
-		$this->db2->from('verifications');
+		$this->db->from('verifications');
 
 		return $this->db2->count_all_results();
 
