ecf9b702515d03d8dcb2cb18755e0ae31d7866ea
Lawal Owolabi Yusuf <bwitlawalyusuf@gmail.com>
2023-07-27 13:45:21 +0100

    Update aws s3 bucket updated.php

--- application/config/aws.php
+++ application/config/aws.php
@@ -1,7 +1,1 @@
 
-<?php
-define("AWS_ACCESS_KEY_ID", "AKIA3X5U4BTTM6R3FGMO");
-define("AWS_ACCESS_KEY_SECRET", "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");
-define("AWS_ACCESS_REGION", "eu-west-1");
-define("AWS_BUCKET_NAME", "dev-rss-uploads");
-?>
