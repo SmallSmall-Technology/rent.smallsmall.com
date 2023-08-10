e4f231ef9cd4de49d90eda603645ade9c6bcdfa6
yusuf <bwitlawalyusuf@gmail.com>
2023-08-10 14:42:30 +0100

    S3 Update

--- application/libraries/Aws_s3.php
+++ application/libraries/Aws_s3.php
@@ -51,13 +51,15 @@ class Aws_s3
             'version' => 'latest',
             'region'  => 'us-east-1', // The region where Parameter Store parameters are stored.
         ]);
-
+    
         try {
             $result = $ssmClient->getParameters([
                 'Names'           => ['ACCESS_KEY_ID', 'ACCESS_KEY_SECRET', 'ACCESS_REGION', 'DEV_BUCKET_NAME'],
                 'WithDecryption'  => true,
             ]);
-
+    
+            $awsConfig = [];
+    
             foreach ($result['Parameters'] as $parameter) {
                 if ($parameter['Name'] === 'ACCESS_KEY_ID') {
                     $this->CI->config->set_item('aws_access_key', $parameter['Value']);
@@ -65,15 +67,27 @@ class Aws_s3
                     $this->CI->config->set_item('aws_secret_key', $parameter['Value']);
                 } elseif ($parameter['Name'] === 'ACCESS_REGION') {
                     $this->CI->config->set_item('aws_region', $parameter['Value']);
+                    $awsConfig['region'] = $parameter['Value']; // Set the region in the AWS SDK config
                 } elseif ($parameter['Name'] === 'DEV_BUCKET_NAME') {
                     $this->CI->config->set_item('aws_bucket', $parameter['Value']);
                 }
             }
+    
+            // Initialize the S3 client with the updated AWS SDK config
+            $this->s3Client = new S3Client([
+                'version'     => 'latest',
+                'credentials' => [
+                    'key'    => $this->CI->config->item('aws_access_key'),
+                    'secret' => $this->CI->config->item('aws_secret_key'),
+                ],
+                'region'      => $awsConfig['region'], // Set the region from the retrieved parameters
+            ]);
+    
         } catch (Exception $e) {
             // Handle error
         }
     }
-
+    
     // The rest of the code remains the same as before.
 
     public function uploadFile($file_path, $destination)
