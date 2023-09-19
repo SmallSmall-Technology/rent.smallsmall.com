99c7d32acd0f2685c17f458c6a6390a3b63b830a
yusuf <bwitlawalyusuf@gmail.com>
2023-09-19 12:11:51 +0100

    Direct Debit"

--- application/models/admin/Admin_model.php
+++ application/models/admin/Admin_model.php
@@ -3743,7 +3743,7 @@ public function fetchSubscribers() {
 
 }
 
-public function directDebitSubsciber()
+public function directDebitSubsciber($data)
 {
 	// Insert data into the 'user_account' table
 	return $this->db->insert('direct_debit_subsciber', $data);
