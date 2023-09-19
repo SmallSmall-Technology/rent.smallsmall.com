e657a82d0f1f0951761c429fe3ca86cd9eaa0da7
yusuf <bwitlawalyusuf@gmail.com>
2023-09-19 13:43:24 +0100

    Added Admin Model

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8689,7 +8689,7 @@ class Admin extends CI_Controller
             );
 
             // Insert the data into the 'direct_debit_subscriber' table using the model
-            $insert_result = $this->Admin_model->directDebitSubscriber($data);
+            $insert_result = $this->admin_model->directDebitSubscriber($data);
 
             if ($insert_result) {
                 // Data inserted successfully
