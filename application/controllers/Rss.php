4267133f4cc36f55e277397fe93d560cda2b9200
Eniola1 <shobowale93@gmail.com>
2023-11-03 09:33:30 +0100

    recurring payment

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -6071,38 +6071,73 @@ public function uploadIdentification($folder)
 		$transID = $newtransID['reference_id']; 
 
 
-		$curl = curl_init();
+		//Add customer to a plan
 
-		curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://api.paystack.co/plan",
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "POST",
-		CURLOPT_POSTFIELDS => array(
-			"name" => "hourly Retainer",
-			"interval" => "hourly",
-			"amount" => 10000
-		),
-		CURLOPT_HTTPHEADER => array(
-			"Authorization: Bearer sk_live_31982685562b561bd7d18d92333cc09ec78952f7",
-			"Cache-Control: no-cache"
-		),
-		)
-		);
+		$url = "https://api.paystack.co/transaction/initialize";
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+		$fields = [
+			'email' => "pidah.t@rentsmallsmall.com",
+			'amount' => "10000",
+			'plan' => "PLN_ed7m7qraxm6lvp9"
+		];
 
-		curl_close($curl);
+		$fields_string = http_build_query($fields);
 
-		if ($err) {
-		echo "cURL Error #:" . $err;
-		} else {
-		echo $response;
-		}
+		//open connection
+		$ch = curl_init();
+		
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, true);
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			"Authorization: sk_live_31982685562b561bd7d18d92333cc09ec78952f7",
+			"Cache-Control: no-cache",
+		));
+		
+		//So that curl_exec returns the contents of the cURL; rather than echoing it
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+		
+		//execute post
+		$result = curl_exec($ch);
+		echo $result;
+
+
+
+		//Create a Plan
+
+		// $curl = curl_init();
+
+		// curl_setopt_array($curl, array(
+		// CURLOPT_URL => "https://api.paystack.co/plan",
+		// CURLOPT_RETURNTRANSFER => true,
+		// CURLOPT_ENCODING => "",
+		// CURLOPT_MAXREDIRS => 10,
+		// CURLOPT_TIMEOUT => 30,
+		// CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		// CURLOPT_CUSTOMREQUEST => "POST",
+		// CURLOPT_POSTFIELDS => array(
+		// 	"name" => "hourly Retainer",
+		// 	"interval" => "hourly",
+		// 	"amount" => 10000
+		// ),
+		// CURLOPT_HTTPHEADER => array(
+		// 	"Authorization: Bearer sk_live_31982685562b561bd7d18d92333cc09ec78952f7",
+		// 	"Cache-Control: no-cache"
+		// ),
+		// )
+		// );
+
+		// $response = curl_exec($curl);
+		// $err = curl_error($curl);
+
+		// curl_close($curl);
+
+		// if ($err) {
+		// echo "cURL Error #:" . $err;
+		// } else {
+		// echo $response;
+		// }
 
 		
 		// $curl = curl_init();
