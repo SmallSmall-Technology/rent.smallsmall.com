57b9b335469619713f7c5e2e7ff8317a25d86c00
yusuf <bwitlawalyusuf@gmail.com>
2023-12-22 01:51:32 +0100

    Notification

--- application/views/rss-partials/new-index.php
+++ application/views/rss-partials/new-index.php
@@ -4,6 +4,21 @@
 <main class="container-fluid">
     <!-- Banner section -->
     <section class="banner">
+        
+        <!-- Notification -->
+        <?php if(isset($notifications) && !empty($notifications)){ ?>
+    <div id="notification-bar-home" class="notification-bar-home">
+      <div id="notification-container">
+        <span id="notification-icn">New</span>
+        <span id="notification-details"><?php echo $notifications['message']; ?></span>
+        <span id="notification-lnk"
+          ><a target="_blank" href="<?php echo $notifications['notification_link']; ?>"><i class="fa fa-angle-right"></i></a
+        ></span>
+      </div>
+    </div>
+    <?php } ?>
+    <!-- End Notification -->
+
         <div class="banner-text">
             <h2>Do you want to pay rent monthly?</h2>
             <p class="font-weight-light">
