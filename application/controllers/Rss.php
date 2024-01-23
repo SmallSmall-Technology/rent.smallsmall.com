6d637bd037e9e54ef9a7ad814ff10e1814da708f
yusuf <bwitlawalyusuf@gmail.com>
2024-01-23 12:18:20 +0100

    Unione API keys updated

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -2537,7 +2537,7 @@ class Rss extends CI_Controller
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -2633,7 +2633,7 @@ class Rss extends CI_Controller
 
 		// Construct the API URL with the required parameters with selzy
 
-		$method = 'https://api.selzy.com/en/api/importContacts?format=json&api_key=6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha&field_names[0]=email&field_names[1]=Name&field_names[2]=email_list_ids&data[0][0]=' . $email . '&data[0][1]=' . $fname . '&data[0][2]=100&field_names[3]=phone&field_names[4]=LastName&data[0][3]=' . $phone . '&data[0][4]=' . $lname;
+		$method = 'https://api.selzy.com/en/api/importContacts?format=json&api_key=6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y&field_names[0]=email&field_names[1]=Name&field_names[2]=email_list_ids&data[0][0]=' . $email . '&data[0][1]=' . $fname . '&data[0][2]=100&field_names[3]=phone&field_names[4]=LastName&data[0][3]=' . $phone . '&data[0][4]=' . $lname;
 
 		$curl = curl_init(); // Initialize a cURL session
 
@@ -2724,7 +2724,7 @@ class Rss extends CI_Controller
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -3383,7 +3383,7 @@ class Rss extends CI_Controller
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -4475,7 +4475,7 @@ class Rss extends CI_Controller
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -5006,7 +5006,7 @@ class Rss extends CI_Controller
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -5971,7 +5971,7 @@ class Rss extends CI_Controller
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -6149,7 +6149,7 @@ class Rss extends CI_Controller
 			$headers = array(
 				'Content-Type' => 'application/json',
 				'Accept' => 'application/json',
-				'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+				'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 			);
 
 			$client = new \GuzzleHttp\Client([
@@ -7917,7 +7917,7 @@ FROMNAME&sender_email=FROMMAIL&subject=SUBJECT
 [filename2]=FILE2&lang=LANG&error_checking=1&metadata[meta1]=
 value1&metadata[meta2]=value2*/
 
-	public function email_test($lname = "RSS", $email = "seuncrowther@yahoo.com", $key = '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha', $subject = 'Test Email', $sender_email = 'test@smallsmall.com', $body = 'hi')
+	public function email_test($lname = "RSS", $email = "seuncrowther@yahoo.com", $key = '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y', $subject = 'Test Email', $sender_email = 'test@smallsmall.com', $body = 'hi')
 	{
 
 		$curl = curl_init();
@@ -8231,7 +8231,7 @@ value1&metadata[meta2]=value2*/
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -8301,7 +8301,7 @@ value1&metadata[meta2]=value2*/
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
@@ -8358,7 +8358,7 @@ value1&metadata[meta2]=value2*/
 		$headers = array(
 			'Content-Type' => 'application/json',
 			'Accept' => 'application/json',
-			'X-API-KEY' => '6tkb5syz5g1bgtkz1uonenrxwpngrwpq9za1u6ha',
+			'X-API-KEY' => '6bgqu7a8bd7xszkz1uonenrxwpdeium56kb1kb3y',
 		);
 
 		$client = new \GuzzleHttp\Client([
