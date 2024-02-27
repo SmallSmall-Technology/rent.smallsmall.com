e3c92cfaa6bb9a96e2fa643918794a1f2340f276
Eniola1 <shobowale93@gmail.com>
2024-02-27 12:20:40 +0100

    pagination

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -4283,7 +4283,7 @@ class Rss extends CI_Controller
 			//$this->session->set(['filterSearch' => $location]);
 		}
 
-		echo $location = $this->session->userdata('fltSearch');
+		$location = $this->session->userdata('fltSearch');
 		
 		$config['total_rows'] = $this->rss_model->getPropertyFilterCounts($location);
 
