5c0ffce46fc2903064577a19ebab04a6f2986524
Eniola1 <shobowale93@gmail.com>
2024-05-30 11:10:07 +0100

    response

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -6382,7 +6382,11 @@ class Rss extends CI_Controller
 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 		CURLOPT_CUSTOMREQUEST => "POST",
 		CURLOPT_POSTFIELDS => $data,
-		CURLOPT_HTTPHEADER => $headers,
+		CURLOPT_HTTPHEADER => array(
+			"accept: application/json",
+			"Content-Type: application/json",
+			"mono-sec-key: test_sk_yviioi5cjr3q1fs4arif"
+		),
 		)
 		);
 
