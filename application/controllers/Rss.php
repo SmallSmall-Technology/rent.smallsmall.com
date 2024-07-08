038365c0635b2acbbdd23bd2557a5a738d229013
Eniola1 <shobowale93@gmail.com>
2024-07-08 10:35:20 +0100

    mono keys

--- application/controllers/Rss.php
+++ application/controllers/Rss.php
@@ -6362,7 +6362,7 @@ class Rss extends CI_Controller
 
 	public function verifyIncome()
 	{
-		$mono_key = 'live_sk_ehewtqp9bmvwvk4flazm';
+		$mono_key = 'test_sk_i3g24pd4urs3akumv8xw';
 
 		$userID = $this->input->post("userID");
 
