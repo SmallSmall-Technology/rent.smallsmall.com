23d8ddcab97e254b003cdaa90e82d51f54fb94d2
Eniola1 <shobowale93@gmail.com>
2024-07-12 11:09:45 +0100

    marital status

--- assets/js/verification.js
+++ assets/js/verification.js
@@ -414,13 +414,13 @@ jQuery(document).ready(function ($) {
 
 				// Redirect to the verification-complete page
 
-				localStorage.removeItem('verificationStorage');
+				// localStorage.removeItem('verificationStorage');
 
-				localStorage.removeItem('rentalBasket');
+				// localStorage.removeItem('rentalBasket');
 
-				window.location.href = baseUrl + "rss/verification-complete";
+				// window.location.href = baseUrl + "rss/verification-complete";
 
-				//alert(JSON.stringify(data, null, 2));
+				alert(JSON.stringify(data, null, 2));
 
 				return false;
 			},
